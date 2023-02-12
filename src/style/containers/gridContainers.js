ade291a92d4479088f6dd56332d23ef3d0159f1b
gerardoMundo <ger.mundo66@gmail.com>
2023-02-11 23:18:50 -0600

    Responsive design for grid container

--- src/style/containers/gridContainers.js
+++ src/style/containers/gridContainers.js
@@ -3,4 +3,8 @@ import styled from 'styled-components';
 export const GridContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
+
+  @media (max-width: 700px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `;
