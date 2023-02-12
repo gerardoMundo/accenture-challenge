dfe95ada90d102fed9d613664b64290530ccf873
gerardoMundo <ger.mundo66@gmail.com>
2023-02-11 20:00:00 -0600

    Created home components and global styles~

--- src/style/styles.js
+++ src/style/styles.js
@@ -1,3 +1,20 @@
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const orangeColor = '#FB8F3D',
+  blackColor = '#040404';
+
+export const GlobalStyles = createGlobalStyle`
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Quicksand', sans-serif;
+}
+   
+a {
+    text-decoration: none;
+}
+`;
 
 export const Title = styled.h1``;
